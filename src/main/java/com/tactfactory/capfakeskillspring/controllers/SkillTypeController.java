d32730dbd883f3fe1fb8a3db557becb09062ccaf
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-11-29 17:21:32 +0100

    progress on controllers

---
+++ src/main/java/com/tactfactory/capfakeskillspring/controllers/SkillTypeController.java
@@ -0,0 +1,23 @@
+package com.tactfactory.capfakeskillspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tactfactory.capfakeskillspring.controllers.base.BaseController;
+import com.tactfactory.capfakeskillspring.models.SkillType;
+
+@Controller
+@RequestMapping(SkillTypeController.BASE_URL)
+public class SkillTypeController extends BaseController<SkillType> {
+
+	public static final String BASE_URL = "/skilltype";
+
+	protected SkillTypeController() {
+		super(SkillType.class);
+	}
+
+	@RequestMapping("/send")
+	public String sendSkillType(){
+		return "coucou";
+	}
+}
---
+++ src/main/java/com/tactfactory/capfakeskillspring/controllers/base/BaseController.java
@@ -0,0 +1,84 @@
+package com.tactfactory.capfakeskillspring.controllers.base;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.tactfactory.capfakeskillspring.dao.interfaces.base.IBaseDAO;
+import com.tactfactory.capfakeskillspring.models.base.BaseEntity;
+
+public abstract class BaseController <T extends BaseEntity> {
+
+	public final static String SHOW_ACTION = "show";
+	public final static String CREATE_ACTION = "create";
+	public final static String UPDATE_ACTION = "update";
+	public final static String DELETE_ACTION = "delete";
+	public final static String LIST_ACTION = "list";
+
+	public final static String REDIRECT = "redirect:";
+
+	public final static String PATH = "/";
+	public final static String PATH_SHOW_FILE = PATH + SHOW_ACTION;
+	public final static String PATH_CREATE_FILE = PATH + CREATE_ACTION;
+	public final static String PATH_UPDATE_FILE = PATH + UPDATE_ACTION;
+	public final static String PATH_DELETE_FILE = PATH + DELETE_ACTION;
+	public final static String PATH_LIST_FILE = PATH + LIST_ACTION;
+
+	public final static String ROUTE_SHOW = "{id}/" + SHOW_ACTION;
+	public final static String ROUTE_CREATE = CREATE_ACTION;
+	public final static String ROUTE_UPDATE = "{id}/" + UPDATE_ACTION;
+	public final static String ROUTE_DELETE = "{id}/" + DELETE_ACTION;
+	public final static String ROUTE_LIST = LIST_ACTION;
+
+	@Autowired
+	private IBaseDAO<T> baseCrud;
+
+	private Class<T> clazz;
+
+	/**
+	 * @return the clazz
+	 */
+	protected Class<T> getClazz() {
+		return clazz;
+	}
+
+	protected BaseController(Class<T> clazz){
+		this.clazz = clazz;
+	}
+
+	public T insertItem(@ModelAttribute T item){
+		baseCrud.create(item);
+		return item;
+	}
+
+	public String updateItem(@ModelAttribute T item){
+		try {
+			baseCrud.update(item);
+		} catch (Exception e) {
+			return "Update failed";
+		}
+		return "Update OK";
+	}
+
+	public String deleteItem(@ModelAttribute T item){
+		try {
+			baseCrud.delete(item);
+		} catch (Exception e) {
+			return "Delete failed";
+		}
+		return "Delete OK";
+	}
+
+	public T getItem(Integer id){
+		T item = null;
+		item = baseCrud.getById(id);
+		return item;
+	}
+
+	public List<T> getItems(){
+		List<T> items = null;
+		items = (List<T>) baseCrud.getAll();
+		return items;
+	}
+}
