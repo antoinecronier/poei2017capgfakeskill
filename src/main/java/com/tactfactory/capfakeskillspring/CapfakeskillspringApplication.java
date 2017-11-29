9e8a2008ca26d71fedc0dc7e7344f70b7abbfc8c
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-11-29 11:28:37 +0100

    initial commit

---
+++ src/main/java/com/tactfactory/capfakeskillspring/CapfakeskillspringApplication.java
@@ -0,0 +1,12 @@
+package com.tactfactory.capfakeskillspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapfakeskillspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapfakeskillspringApplication.class, args);
+	}
+}
